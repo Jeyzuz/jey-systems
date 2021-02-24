fdccf5041f166b7f00a2fc99481af9712f869394
Matt Walters <mattcw96@gmail.com>
2021-02-23 19:31:42 -0500

    blank project

---
+++ desktop/src/jey/demo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package jey.demo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import jey.demo.DemoMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DemoMain(), config);
+	}
+}
